f129d251bd46f2759e9ebda9c7b06c98f6317162
tumblrkaga <tumblrkaga@gmail.com>
2019-02-12 15:45:43 +0100

    Initial1

---
+++ exercise1.py
@@ -0,0 +1,16 @@
+
+#Text
+print("Hello World")
+print ("Hello Again")
+print ("I like typing this")
+print ("This is fun")
+print ('yay! Printing')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this')
+
+#Math
+
+print(100 - 25 * 3.0 % 4)
+print(3 + 2 + 1 - 5 + 4.0% 2 - 1.0 //4 + 6)
+print(7.0 / 4.0 , 7/4)
+print(3 +2 < 5-7)
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+space_in_car = 9.0
+cars = 30
+passengers_per_car = space_in_car / cars
+print(passengers_per_car)
+space_in_car = 9
+cars = 30
+passengers_per_car = space_in_car / cars
+print(passengers_per_car)
+
